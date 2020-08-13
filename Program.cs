52a9ff3b51279730b1fe4297b7c0ea1466a0655f
Daniel Labios <labiosd@gmail.com>
2020-08-12 20:36:42 -0400

    Adventure Mode Completed!

--- Program.cs
+++ Program.cs
@@ -43,6 +43,63 @@ namespace AllCardsOnDeckCS
 
             Console.WriteLine($"First Card: {deck[0]}");
             Console.WriteLine($"Second Card: {deck[1]}");
+
+            // |Adventure| distribute all cards to 2 players with first 2 cards going to playerHand1
+
+            System.Threading.Thread.Sleep(3000);
+
+            Console.WriteLine("distributing cards amongst 2 playerHands");
+            var playerHand1 = new List<string>();
+            var playerHand2 = new List<string>();
+            int distributeCount = 0;
+            foreach (string card in deck)
+            {
+                if (distributeCount < 2)
+                {
+                    playerHand1.Add(deck[distributeCount]);
+                    distributeCount++;
+                }
+                else
+                {
+                    if (playerHand1.Count == playerHand2.Count)
+                    {
+                        playerHand1.Add(deck[distributeCount]);
+                    }
+                    else
+                    {
+                        playerHand2.Add(deck[distributeCount]);
+                    }
+                    distributeCount++;
+                }
+            }
+            // showing both players hand to user for verification of working code
+
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Playerhand1");
+            Console.WriteLine($"Number of cards in hand: {playerHand1.Count}");
+            Console.WriteLine("Cards in hand:");
+            int cardCount = 0;
+            foreach (string card in playerHand1)
+            {
+                Console.WriteLine(playerHand1[cardCount]);
+                cardCount++;
+            }
+            Console.WriteLine("Playerhand2");
+            Console.WriteLine($"Number of cards in hand: {playerHand2.Count}");
+            Console.WriteLine("Cards in hand:");
+            cardCount = 0;
+            foreach (string card in playerHand2)
+            {
+                Console.WriteLine(playerHand2[cardCount]);
+                cardCount++;
+            }
+            Console.WriteLine("Complete! Here are their hands!");
+
+
+
+
+
+
         }
     }
 }
